27187791466a2478c2377d97a32b76f2e5089c3a
Kristaspahilari <66671551+Kristaspahilari@users.noreply.github.com>
2021-08-27 11:53:28 +0200

    Add files via upload

--- northwind/app.py
+++ northwind/app.py
@@ -4,6 +4,17 @@ import database
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def suppliers():
     suppliers = database.get_all_suppliers()
     return render_template('index.html', suppliers=suppliers)
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    supplier = database.get_products(supplier_id)
+    return render_template('products.html', products=products, supplier=supplier)
+
+@app.route("/categories")
+def categories():
+    categories = database.get_categories()
+    return render_template('category.html', categories=categories)
