89f4b7ec5c4e1b56c4901fa3c56225a9e6cdd0d9
Kristaspahilari <66671551+Kristaspahilari@users.noreply.github.com>
2021-09-02 14:16:52 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import database
+
+app = Flask(__name__)
+
+@app.route("/")
+def suppliers():
+    suppliers = database.get_all_suppliers()
+    return render_template('index.html', suppliers=suppliers)
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    supplier = database.get_products(supplier_id)
+    return render_template('products.html', products=products, supplier=supplier)
+
+@app.route("/categories")
+def categories():
+    categories = database.get_categories()
+    return render_template('category.html', categories=categories)
+
+@app.route("/categories/<int:category_id>")
+def categoryproducts(category_id):
+    categories = database.get_categories()
+    categoryproducts = database.get_categories_products(category_id)
+
+    
+    return render_template('categoryproducts.html', categoryproducts=categoryproducts, categories=categories)
